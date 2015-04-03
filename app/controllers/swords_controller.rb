59ba0c297212f599bc481638db55c22c4ff72b62
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2015-04-02 21:54:35 -0700

    making queries case insensitive

--- app/controllers/swords_controller.rb
+++ app/controllers/swords_controller.rb
@@ -4,18 +4,23 @@ class SwordsController < ApplicationController
     if params[:price] && params[:price].length > 0
       @swords = @swords.where("price <= ?", params[:price])
     end
+
     if params[:length] && params[:length].length > 0
        @swords = @swords.where("total_length >= ?", params[:length])
     end
+
     if params[:grip] && params[:grip].length > 0
       @swords = @swords.where("hilt_length >= ?", params[:grip])
     end
+
     if params[:instock]
-      @swords = @swords.where("in_stock == ?", 'In Stock')
+      @swords = @swords.where("lower(in_stock) == ?", 'in stock')
     end
+
     if params[:blunt]
-      @swords = @swords.where("edge in (?, ?)", "Blunt", "Unsharpened")
+      @swords = @swords.where("lower(edge) in (?, ?)", "blunt", "unsharpened")
     end
+    
     if params[:sort]
       @swords = @swords.order(params[:sort])
     end
